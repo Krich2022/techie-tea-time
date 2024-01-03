d1cc6769ddb6a5604e2f1d8c11a5863362d1d128
Krich2020 <keston@therichfamily.net>
2024-01-03 12:35:10 -0700

    debugging

--- public/js/post.js
+++ public/js/post.js
@@ -27,6 +27,7 @@ const getPost = async () => {
     const userData = await user.json();
     console.log(userData);
     console.log("hello");
+    console.log(currentUserId === postData);
 
     console.log(currentUserId);
     console.log(postData);
